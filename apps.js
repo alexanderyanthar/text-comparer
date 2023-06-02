58fa6bd58ad7a13a0bf1dcc0acc0126bcaf01e29
alexanderyanthar <alexander.yanthar@outlook.com>
2023-06-02 13:08:56 -0400

    added parcel to deploy on netlify

--- apps.js
+++ apps.js
@@ -1,3 +1,5 @@
+import { diffWords } from "diff";
+
 function compareText() {
     const text1 = document.getElementById('text1').value;
     const text2 = document.getElementById('text2').value;
@@ -5,7 +7,7 @@ function compareText() {
     const filteredText1 = text1.replace(/[\{\[].*?[\}\]]/g, '');
     const filteredText2 = text2.replace(/[\{\[].*?[\}\]]/g, '');
 
-    const d = Diff.diffWords(filteredText1, filteredText2);
+    const d = diffWords(filteredText1, filteredText2);
     const diffOutput = document.getElementById('diffOutput');
     diffOutput.innerHTML = '';
 
