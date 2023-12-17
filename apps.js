021cb8fc2f2bb5da992433a2b71c8102a4eca659
alexanderyanthar <alexander.yanthar@outlook.com>
2023-12-17 13:43:14 -0500

    changed styling

--- apps.js
+++ apps.js
@@ -1,32 +1,30 @@
 import { diffWords } from "diff";
 
 function compareText() {
-    const text1 = document.getElementById('text1').value;
-    const text2 = document.getElementById('text2').value;
+  const text1 = document.getElementById("text1").value;
+  const text2 = document.getElementById("text2").value;
 
-    const filteredText1 = text1.replace(/[\{\[].*?[\}\]]/g, '');
-    const filteredText2 = text2.replace(/[\{\[].*?[\}\]]/g, '');
+  const filteredText1 = text1.replace(/[\{\[].*?[\}\]]/g, "");
+  const filteredText2 = text2.replace(/[\{\[].*?[\}\]]/g, "");
 
-    const d = diffWords(filteredText1, filteredText2);
-    const diffOutput = document.getElementById('diffOutput');
-    diffOutput.innerHTML = '';
+  const d = diffWords(filteredText1, filteredText2);
+  const diffOutput = document.getElementById("diffOutput");
+  diffOutput.innerHTML = "";
 
-    d.forEach(item => {
-        const span = document.createElement('span');
-        span.textContent = item.value;
+  d.forEach((item) => {
+    const span = document.createElement("span");
+    span.textContent = item.value;
 
-        if (item)
+    if (item)
+      if (item.added) {
+        span.classList.add("added");
+      } else if (item.removed) {
+        span.classList.add("removed");
+      }
 
-        if (item.added) {
-            span.classList.add('added');
-        } else if (item.removed) {
-            span.classList.add('removed');
-        }
-
-        diffOutput.appendChild(span);
-    })
+    diffOutput.appendChild(span);
+  });
 }
 
-
-const compareButton = document.getElementById('compareButton');
-compareButton.addEventListener('click', compareText);
+const compareButton = document.getElementById("compareButton");
+compareButton.addEventListener("click", compareText);
